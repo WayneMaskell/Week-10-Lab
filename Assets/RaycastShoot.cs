3f606970c1b113f25fc4b7856c6b53dc28d9bf1c
WayneMaskell <152607117+WayneMaskell@users.noreply.github.com>
2024-02-05 13:43:17 +0000

    Labs 10.1  commit
    10.1 labs complete

---
+++ Assets/RaycastShoot.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastShoot : MonoBehaviour
+{
+    public int gunDamage = 1;
+    public float fireRate = 0.25f;
+    public float weaponRange = 50f;
+    public float hitForce = 100f;
+    public Transform gunEnd;
+    public Camera fpsCam;
+    private AudioSource gunAudio;
+    private WaitForSeconds shotDuration = new WaitForSeconds(0.7f);
+    private LineRenderer laserLine;
+    private float nextFire;
+    // Start is called before the first frame update
+    void Start()
+    {
+        laserLine = GetComponent<LineRenderer>();
+        gunAudio = GetComponent<AudioSource>();
+        fpsCam = GetComponentInParent<Camera>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        laserLine.SetPosition(0, gunEnd.position);
+
+        Vector3 rayOrigin = fpsCam.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, 0));
+        if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit ,weaponRange)) 
+        
+        {
+            laserLine.SetPosition(1, hit.point);
+        }
+        else 
+        {
+            laserLine.SetPosition (1, fpsCam.transform.forward);
+        }
+        if (Input.GetButtonDown("Fire1") && Time.time > nextFire) 
+        {
+            nextFire = Time.time + fireRate;
+            StartCoroutine(ShotEffect());
+        }
+        
+    }
+
+    private IEnumerator ShotEffect() 
+    {
+        gunAudio.Play();
+        laserLine.enabled = true;
+        yield return shotDuration;
+        laserLine.enabled = false;
+    }
+}
